3a414163c76696cc2b74019ef7794ecc62728500
gabrieldoviolao <gabrielmelo@id.uff.br>
2017-02-20 15:48:45 -0300

    For loop printing cardinal numbers from 1 to10

--- index.php
+++ index.php
@@ -2,12 +2,16 @@
 
 	echo "<h1>"."What's up, galera???"."</h1>";
 	for($i = 0; $i<=10; $i++){
-		echo "echoing 10 times<br>";
-		echo "$i <br>";
+		if($i == 0)
+		echo "echoing for the 1st time<br>";
+		else if($i == 1)
+		echo "echoing for the 2nd time<br>";
+		else if($i == 2)
+		echo "echoing for the 3rd time<br>";
+		else 
+		echo "echoing for the ".($i+1)."th time<br>";
+	//whithout parentheses, concatenation won't work.($i+1).
+		
 	}
-	echo "line 1<br>";//Without br, there's no line break 
-	echo "line 2";
-	//"Hello, world" script in PHP
-	//This is how concatenation of strings is done in PHP: 
-	//"string one "." string two" == "string one  string two" sdafsds
+	
 ?>
