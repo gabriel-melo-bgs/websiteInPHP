58b1c6e2a62b736439d219f8461f982ee7250fc8
gabriel-melo-bgs <gabriel.melo.bgs@gmail.com>
2017-02-20 15:20:49 -0300

    Hello, world; for loop; br tag

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+	echo "<h1>"."What's up, galera???"."</h1>";
+	for($i = 0; $i<=10; $i++){
+		echo "echoing 10 times<br>";
+		echo "$i <br>";
+	}
+	echo "line 1<br>";//Without br, there's no line break 
+	echo "line 2";
+	//"Hello, world" script in PHP
+	//This is how concatenation of strings is done in PHP: 
+	//"string one "." string two" == "string one  string two" sdafsds
+?>
