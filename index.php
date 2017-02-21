6e1698037a1e29af406e5e3791ca6b20b8fa254a
gabrieldoviolao <gabrielmelo@id.uff.br>
2017-02-21 10:09:55 -0300

    header; count($myArray);  using \ for variables to be echoed

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 	echo "<title>Why not add a title?!</title>"; 
-	echo "<h1>"."What's up, galera???"."</h1>";//just to try some concatenation
+	echo "<header><h1>Let's put a <strong>header</strong> here too!</h1></header>"; //It's kind of a title for the page
 
 	$myArray = array("1st thing", "2nd thing", "last thing"); //This is wrong!!! $myArray = new array("bla", "bla", "bla");
+	//$myArray2 = 
 	for($i = 0; $i<=10; $i++)
 	{
 		if($i<3){
@@ -10,8 +11,11 @@
 			echo "<h2>"; //This will be concatenated with the following echo output
 			echo $myArray[$i]."<br>";//just added a little line break using PHP's weird concatenation syntax again
 			echo "</h2>";
-
 		}
 	}
-	
+	echo "<h3>This is how I get the length of an array: <br>
+				  count(\$myArray): " . (count($myArray)) . "</h3>"; 
+				  //Without \, echo is not able to output the string "count($myArray)"
+				  //I was given an error when I tried outputting count($myArray) on line 18
+
 ?>
