611c7008b085d0eb69ff6cd0f97b8e169e8f3440
gabrieldoviolao <gabrielmelo@id.uff.br>
2017-02-22 13:41:56 -0300

    Fixing some stuff of the previous commit

--- index.php
+++ index.php
@@ -2,20 +2,31 @@
 	echo "<title>Why not add a title?!</title>"; 
 	echo "<header><h1>Let's put a <strong>header</strong> here too!</h1></header>"; //It's kind of a title for the page
 
-	$myArray = array("1st thing", "2nd thing", "last thing"); //This is wrong!!! $myArray = new array("bla", "bla", "bla");
-	//$myArray2 = 
+	$myArray = array("1st thing", "2nd thing", "last thing"); //The following is wrong $myArray = new array("bla", "bla", "bla");
+	
 	for($i = 0; $i<=10; $i++)
 	{
-		if($i<3){
+		if($i<count($myArray)){ //I can use the array size instead
 
 			echo "<h2>"; //This will be concatenated with the following echo output
 			echo $myArray[$i]."<br>";//just added a little line break using PHP's weird concatenation syntax again
 			echo "</h2>";
 		}
 	}
-	echo "<h3>This is how I get the length of an array: <br>
-				  count(\$myArray): " . (count($myArray)) . "</h3>"; 
-				  //Without \, echo is not able to output the string "count($myArray)"
-				  //I was given an error when I tried outputting count($myArray) on line 18
+
+	$arr = array("one", "two", "three"); 
+	//which is just like $arr = array(0 => "one", 1 => "two", 2 => "three");
+
+
+	foreach ($arr as $key => $value) { //$arr is just the array in which I'm looping 
+    	echo "Key: $key; Value: $value<br />\n";
+	}
+	echo "<br />\n";
+
+	$chordHash = array('C9' => "C E G D", 'Dm7/9' => "D F A C E");
+
+	foreach ($chordHash as $chord => $notes) {
+		echo "Chord: $chord <br />Notes: $notes <br /><br />";
+	}
 
 ?>
